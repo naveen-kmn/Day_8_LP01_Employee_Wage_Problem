3dda8b12f6c4b0a40feb4d4855036aacbac00a15
Naveen-kmn <kmnaveenmech@gmail.com>
2021-10-29 14:15:37 +0530

    Added : UC4 is added

--- src/com/blp/employee_wage_problem/EmployeeWageProblem.java
+++ src/com/blp/employee_wage_problem/EmployeeWageProblem.java
@@ -10,20 +10,23 @@ public class EmployeeWageProblem
     {
         int empHours;
         int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-        if ( empCheck == EMP_FULL_TIME_HOUR )
+        switch (empCheck)
         {
-            empHours = 8;
-            System.out.println("Employee is Present");
-        }
-        else if ( empCheck == EMP_PART_TIME_HOUR )
-        {
-            empHours = 4;
-            System.out.println("Employee is Part time present");
-        }
-        else
-        {
-            empHours = 0;
-            System.out.println("Employee is Absent");
+            case EMP_FULL_TIME_HOUR ->
+                    {
+                empHours = 8;
+                System.out.println("Employee is present for full time");
+                    }
+            case EMP_PART_TIME_HOUR ->
+                    {
+                empHours = 4;
+                System.out.println("Employee is present for half time");
+                    }
+            default ->
+                    {
+                empHours = 0;
+                System.out.println("Employee is absent");
+                     }
         }
         int empWage = (EMP_WAGE_PER_HOUR * empHours);
         System.out.println("Employee Wage = " + empWage);
