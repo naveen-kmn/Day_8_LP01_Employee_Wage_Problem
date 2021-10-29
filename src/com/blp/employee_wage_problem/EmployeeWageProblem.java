9b3fa1b1471093ab3c598a08dadb05586add0255
Naveen-kmn <kmnaveenmech@gmail.com>
2021-10-29 14:10:09 +0530

    Added : UC2 is added

--- src/com/blp/employee_wage_problem/EmployeeWageProblem.java
+++ src/com/blp/employee_wage_problem/EmployeeWageProblem.java
@@ -2,17 +2,25 @@ package com.blp.employee_wage_problem;
 
 public class EmployeeWageProblem
 {
+    public static final int EMP_WAGE_PER_HOUR = 20;
+
     public static void main(String[] args)
     {
+        int empHours;
         int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
 
         if ( empCheck == 1 )
         {
+            empHours = 8;
             System.out.println("Employee is Present");
         }
         else
         {
+            empHours = 0;
             System.out.println("Employee is Absent");
         }
+
+        int empWage = (EMP_WAGE_PER_HOUR * empHours);
+        System.out.println("Employee Daily Wage = " + empWage);
     }
 }
