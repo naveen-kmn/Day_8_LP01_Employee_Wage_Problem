8fa9c982e662a6a826a9859e06ae63dec32e8bca
Naveen-kmn <kmnaveenmech@gmail.com>
2021-10-29 14:26:54 +0530

    Added : UC6 is added

--- src/com/blp/employee_wage_problem/EmployeeWageProblem.java
+++ src/com/blp/employee_wage_problem/EmployeeWageProblem.java
@@ -5,32 +5,38 @@ public class EmployeeWageProblem
     public static final int EMP_FULL_TIME_HOUR = 1;
     public static final int EMP_PART_TIME_HOUR = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
-    public static final int EMP_WORKING_DAY_MONTH = 20;
+    public static final int NUM_WORKING_DAY_MONTH = 20;
+    public static final int EMP_MONTHLY_WORKING_HOUR = 100;
 
     public static void main(String[] args)
     {
         int empHours;
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
-        switch (empCheck)
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalEmpHrs <= EMP_MONTHLY_WORKING_HOUR && totalWorkingDays < NUM_WORKING_DAY_MONTH)
         {
-            case EMP_FULL_TIME_HOUR ->
-                    {
-                        empHours = 8;
-                        System.out.println("Employee is present for full time");
-                    }
-            case EMP_PART_TIME_HOUR ->
-                    {
-                        empHours = 4;
-                        System.out.println("Employee is present for half time");
-                    }
-            default ->
-                    {
-                        empHours = 0;
-                        System.out.println("Employee is absent");
-                    }
+            totalWorkingDays++;
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            switch (empCheck)
+            {
+                case EMP_FULL_TIME_HOUR -> {
+                            empHours = 8;
+                            System.out.println("Employee is present for full time");
+                }
+                case EMP_PART_TIME_HOUR -> {
+                            empHours = 4;
+                            System.out.println("Employee is present for half time");
+                }
+                default ->
+                        {
+                            empHours = 0;
+                            System.out.println("Employee is absent");
+                        }
+            }
+            totalEmpHrs += empHours;
+            System.out.println("Day:" + totalWorkingDays + "Emp Hr:" + empHours);
         }
-            int empWage = (EMP_WAGE_PER_HOUR * empHours);
-            int totalEmpWage = (empWage * EMP_WORKING_DAY_MONTH);
-            System.out.println("Employee Wage = " + totalEmpWage);
+        int totalEmpWage = totalEmpHrs * EMP_WAGE_PER_HOUR;
+        System.out.println("Total Emp Wage = " + totalEmpWage);
     }
 }
