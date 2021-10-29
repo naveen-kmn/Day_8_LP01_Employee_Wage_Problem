55308a2c772bd9ad883a7bc9682ee1097d465d27
Naveen-kmn <kmnaveenmech@gmail.com>
2021-10-29 14:18:17 +0530

    Added : UC5 is added

--- src/com/blp/employee_wage_problem/EmployeeWageProblem.java
+++ src/com/blp/employee_wage_problem/EmployeeWageProblem.java
@@ -5,6 +5,7 @@ public class EmployeeWageProblem
     public static final int EMP_FULL_TIME_HOUR = 1;
     public static final int EMP_PART_TIME_HOUR = 2;
     public static final int EMP_WAGE_PER_HOUR = 20;
+    public static final int EMP_WORKING_DAY_MONTH = 20;
 
     public static void main(String[] args)
     {
@@ -14,21 +15,22 @@ public class EmployeeWageProblem
         {
             case EMP_FULL_TIME_HOUR ->
                     {
-                empHours = 8;
-                System.out.println("Employee is present for full time");
+                        empHours = 8;
+                        System.out.println("Employee is present for full time");
                     }
             case EMP_PART_TIME_HOUR ->
                     {
-                empHours = 4;
-                System.out.println("Employee is present for half time");
+                        empHours = 4;
+                        System.out.println("Employee is present for half time");
                     }
             default ->
                     {
-                empHours = 0;
-                System.out.println("Employee is absent");
-                     }
+                        empHours = 0;
+                        System.out.println("Employee is absent");
+                    }
         }
-        int empWage = (EMP_WAGE_PER_HOUR * empHours);
-        System.out.println("Employee Wage = " + empWage);
+            int empWage = (EMP_WAGE_PER_HOUR * empHours);
+            int totalEmpWage = (empWage * EMP_WORKING_DAY_MONTH);
+            System.out.println("Employee Wage = " + totalEmpWage);
     }
 }
